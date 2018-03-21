bf9e214558727f574d3c632ed9adac7028a9fd23
Akshay Gohel <akshay.gohel@nihilent.com>
2018-03-21 14:47:41 +0530

    basic setup of Project

---
+++ app/src/main/java/com/example/akshaygohel/myapplication/Test2.java
@@ -0,0 +1,22 @@
+package com.example.akshaygohel.myapplication;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by akshay.gohel on 20-03-2018.
+ */
+
+public class Test2 extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        System.out.print("Hello");
+    }
+
+}
